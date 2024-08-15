f055629756d42fd0fcb9a144d1f1afec57a0f275
Gabi <playcs.gabi@yahoo.com>
2024-08-15 15:57:37 +0300

    first commit

---
+++ java/Sesiunea1/Challenge.java
@@ -0,0 +1,175 @@
+package Sesiunea1;
+
+import java.util.Scanner;
+
+public class Challenge {
+
+    public static void main(String[] args) {
+     cha9();
+
+    }
+//    1. Declare and initialize variables of different data types (int, double, char,
+//     boolean) to store various values (e.g., age, height, first letter of your name, isStudent).
+
+//     cha1(); //This need write in main class.
+
+    public static void cha1(){
+        int a = 23;
+        int b = 14;
+        int c = 43;
+
+        int rezAdd = a + b + c;
+        System.out.println("Rezultatul adunari = " + rezAdd);
+
+        int rezSub = c - (a - b);
+        System.out.println("Rezultatul scaderii = " + rezSub);
+
+        int rezMul = a * c * b;
+        System.out.println("Rezultatul înmultirii = " + rezMul);
+
+        int rezDiv = c / ( a / b);
+        System.out.println("Rezultatul împartirii = " + rezDiv);
+
+    }
+//    2. Perform simple arithmetic operations using variables of numeric data types
+//    (addition,subtraction, multiplication, division).
+
+//    cha2(); // This need write in main class.
+
+    public static void cha2() {
+
+        Scanner inputScanner = new Scanner(System.in);
+
+        System.out.println("Scrie numele tau: ");
+        String myName = (inputScanner.nextLine());
+
+        System.out.println("Varsta ta este:");
+        int myAge = inputScanner.nextInt();
+
+        System.out.println("Inaltimea ta este: ");
+        double myW = inputScanner.nextDouble();
+
+        System.out.println("Initiala tatalui tau este: ");
+        char firstLetter;
+        firstLetter = inputScanner.next().charAt(0);
+
+
+        boolean myBoolean = (myAge > 18);
+
+
+        System.out.println("Hello, my name is " + myName + " " + "I have " +
+                myAge + " age " + ",the height of  " + myW + ", my ftather's initial is " +
+                firstLetter + " " + " and is  " + " " + myBoolean + " " + "I am major.");
+
+
+        inputScanner.close();
+    }
+//    3. Concatenate strings using the + operator to create new strings.
+
+//    cha3(); // This need write in main class.
+
+    public static void cha3(){
+        System.out.println("Prima strofa din poezia Luceafarrul, de Mihai Eminescu.");
+        String a = "A fost ";
+        String b = "odata ca-n povesti, ";
+        String vers1 =a.concat(b);
+
+        String c = "ca niciodata, ";
+        String vers2 = a.concat(c);
+
+        String d = "Din rude mari";
+        String e = " imparatesti, ";
+        String vers3 = d.concat(e);
+
+        String f = "O prea ";
+        String g = "frumoasa fata.";
+        String vers4 = f.concat(g);
+
+        System.out.println(vers1 +  vers2 +  vers3 +  vers4);
+
+    }
+//    4. Convert between different numeric data types (e.g., int to double, double to int).
+
+//    cha4(); // This need to write to the  main class.
+     public static void cha4() {
+
+         int myInt1 = 15;
+         double myDouble1;
+         myDouble1 = myInt1;
+         System.out.println("Conversia din int in double = " + myDouble1);
+
+         int myInt2;
+         double myDouble2 = 1234.312d;
+         myInt2 = (int) myDouble2;
+         System.out.println("Conversia din double in int = " + myInt2);
+
+
+    }
+//    5. Create a program that calculates the area of a rectangle, given its length and width.
+
+//      cha5(); This need to write to the main class.
+
+        public static void cha5() {
+
+        int length = 14; // meter
+        int width = 20; // meter
+        int area = length * width;
+            System.out.println("Area of rectangle is = " + area + "meter." );
+
+    }
+//    6. Write a program that calculates the average of three numbers.
+
+//    cha6(); // This need to write to the main class.
+
+       public static void cha6(){
+         int num1 = 21;
+         int num2 = 62;
+         int num3 = 32;
+         int average = (num1 + num2 + num3) / 3;
+         System.out.println("Average of three numbers = " + average);
+
+    }
+//    7. Create a program that calculates the body mass index (BMI) given weight and height.
+//      cha7(); // This need to write to the main class.
+        public static void cha7() {
+        int weight = 69; // kg
+        double height = 1.81; // meter
+        double BMI = weight / Math.pow(height,2);
+
+
+            System.out.println("Gabi have " + weight + "kg, " + height +
+                    "m" + " and have BMI " + BMI + "." );
+
+    }
+//    8. Create a program that calculates the circumference and area of a circle, given its
+//    radius.
+
+//      cha8(); // This need write in main class.
+
+    public static void cha8() {
+
+        int radius = 15; // meter
+        int diameter = radius * 2;
+        double pi =  3.14;
+        double circumference = pi * diameter;
+        double area = pi * Math.pow(radius, 2);
+
+        System.out.println("Our circle have radius is " + radius + "m" + " ,diameter is " +
+                diameter + " ,circumference is " + circumference + " and area is " + area);
+
+    }
+
+//9. Write a program that converts temperature from Celsius to Fahrenheit.
+
+//   cha9(); // This need write in main class.
+
+    public static void cha9() {
+
+        double celsius = 23, fahrenheit = 0.0;
+        fahrenheit =(celsius * 1.8) +32;
+        System.out.println("Conversion from celsius to fahrenheit is " + fahrenheit + ".");
+
+    }
+}
+
+
---
+++ java/Sesiunea1/Homework.java
@@ -0,0 +1,89 @@
+package Sesiunea1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Homework {
+    public static void main(String[] args) {
+
+
+   }
+//        1. Simple Output Program: Write a program that prints your name, age,
+//    and favorite color on three separate lines.
+
+//     homework1(args); // This need write in main class.
+
+    public static void homework1(String[] args) {
+
+        System.out.println(Arrays.toString(args));
+
+        System.out.println("Numele meu este " + args[0] + ".");
+        System.out.println("Am " + args[1] + " " + "de ani.");
+        System.out.println("Imi place culoarea " + args[2] + ".");
+    }
+//     2. Using Variables: Write a program that defines four different variables
+//   (an integer, a double, a character, and a string), assigns them values, and then prints them out.
+
+//    homework2(); // This need to write in main class.
+
+    public static void homework2() {
+
+        int myInt = 26;
+        char myChar = 'a';
+        double myDouble = 34.4d;
+        String myText = "have";
+
+        System.out.println("I  " + myText + " " + myInt + " " + "age " + " and " +
+                " " + "Vasile " + myText + " " + myDouble + " " + myChar + "ge.");
+
+
+    }
+//    3. Operations: Write a program that performs various operations
+//    (addition, subtraction, multiplication, division, and modulus)
+//    on two numbers. The numbers can be hard-coded into the program. Print the result of each operation.
+
+//      homework3 // This need write to main class.
+
+    public static void homework3() {
+
+        int a, b;
+        a = 6;
+        b = 9;
+        int rezAddition = a + b;
+        System.out.println("rezAddition = " + rezAddition);
+        int rezSubs = b - a;
+        System.out.println("rezSubs = " + rezSubs);
+        int rezMul = a * b;
+        System.out.println("rezMul = " + rezMul);
+        int rezDiv = b / a;
+        System.out.println("rezDiv = " + rezDiv);
+        int rezMod = b % a;
+        System.out.println("rezMod = " + rezMod);
+
+
+    }
+//     4. Simple Input/Output: Write a program that asks the user to enter two numbers,
+//     performs addition on those numbers, and prints out the result.
+
+//    homework4 // This need to write in main class.
+
+    public static void homework4() {
+
+        Scanner inputScanner = new Scanner(System.in);
+
+        System.out.println("Scrie primul numar: ");
+        int primulNumar = Integer.parseInt(inputScanner.nextLine());
+
+        System.out.println("Introduceti al doilea numar: ");
+        int numarulDoi = Integer.parseInt(inputScanner.nextLine());
+
+        int rezult = primulNumar + numarulDoi;
+        System.out.println("Adunarea a doua numere " + rezult);
+
+        inputScanner.close();
+
+
+    }
+
+
+}
